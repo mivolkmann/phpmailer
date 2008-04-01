ef3bdc2c2462fe6c5ef2153d3e6088d7ee0f24e8
coolbru <coolbru@f22d3040-cd0d-0410-a2d0-ced8d2d2406b>
2008-04-01 10:34:00 +0000

    Import version 2.0.0 code Give everything an Id attribute
    git-svn-id: https://phpmailer.svn.sourceforge.net/svnroot/phpmailer/trunk/phpmailer@165 f22d3040-cd0d-0410-a2d0-ced8d2d2406b

--- language/phpmailer.lang-no.php
+++ language/phpmailer.lang-no.php
@@ -1,20 +1,23 @@
 <?php
 /**
  * PHPMailer language file.
- * Norwegian Version provided by Ketil - stadskle in ticket #1471311
+ * Norwegian Version
  */
 
 $PHPMAILER_LANG = array();
-$PHPMAILER_LANG["provide_address"] = 'Du må ha med minst en mottager adresse.';
-$PHPMAILER_LANG["mailer_not_supported"] = ' Valgte e-post sender er ikke støttet.';
+
+$PHPMAILER_LANG["provide_address"] = 'Du må ha med minst en' .
+                                     'mottager adresse.';
+$PHPMAILER_LANG["mailer_not_supported"] = ' mailer er ikke supportert.';
 $PHPMAILER_LANG["execute"] = 'Kunne ikke utføre: ';
-$PHPMAILER_LANG["instantiate"] = 'Kunne ikke initialisere e-post funksjonen.';
-$PHPMAILER_LANG["authenticate"] = 'SMTP Feil: Kunne ikke autentisere.';
+$PHPMAILER_LANG["instantiate"] = 'Kunne ikke instantiate mail funksjonen.';
+$PHPMAILER_LANG["authenticate"] = 'SMTP Feil: Kunne ikke authentisere.';
 $PHPMAILER_LANG["from_failed"] = 'Følgende Fra feilet: ';
-$PHPMAILER_LANG["recipients_failed"] = 'SMTP Feil: Følgende mottagere feilet: ';
+$PHPMAILER_LANG["recipients_failed"] = 'SMTP Feil: Følgende' .
+                                       'mottagere feilet: ';
 $PHPMAILER_LANG["data_not_accepted"] = 'SMTP Feil: Data ble ikke akseptert.';
-$PHPMAILER_LANG["connect_host"] = 'SMTP Feil: Kunne ikke koble til SMTP tjener.';
+$PHPMAILER_LANG["connect_host"] = 'SMTP Feil: Kunne ikke koble til SMTP host.';
 $PHPMAILER_LANG["file_access"] = 'Kunne ikke få tilgang til filen: ';
 $PHPMAILER_LANG["file_open"] = 'Fil feil: Kunne ikke åpne filen: ';
-$PHPMAILER_LANG["encoding"] = 'Ukjent enkoding: ';
+$PHPMAILER_LANG["encoding"] = 'Ukjent encoding: ';
 ?>
